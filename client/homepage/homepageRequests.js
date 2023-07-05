2328f7884f31502395079ac0ada40d7401faa0a8
ajearle11 <ajearle9@gmail.com>
2023-07-05 12:47:08 +0100

    Added signin and signup logic

--- client/homepage/homepageRequests.js
+++ client/homepage/homepageRequests.js
@@ -1,31 +1,31 @@
-// document.querySelector("#login-submit").addEventListener("click", async () => {
-//   const password = document.querySelector("#password-login").value;
-//   const username = document.querySelector("#email-login").value;
+document.querySelector("#login-submit").addEventListener("click", async () => {
+  const password = document.querySelector("#password-login").value;
+  const username = document.querySelector("#email-login").value;
 
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       email: username,
-//       password: password,
-//     }),
-//   };
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: username,
+      password: password,
+    }),
+  };
 
-//   const response = await fetch("http://localhost:3002/users/login", options);
-//   const data = await response.json();
-//   console.log(data);
+  const response = await fetch("http://localhost:3002/users/login", options);
+  const data = await response.json();
+  console.log(data);
 
-// if (response.status == 200) {
-//   console.log("yoo");
-//   window.location.assign("homepage.html");
-// } else {
-//   alert(data.error);
-//   console.log("naaa");
-// }
-// });
+  if (response.status == 200) {
+    console.log("yoo");
+    window.location.assign("homepage.html");
+  } else {
+    alert(data.error);
+    console.log("naaa");
+  }
+});
 
 document
   .querySelector("#register-submit")
@@ -33,7 +33,7 @@ document
     const password = document.querySelector("#password-register").value;
     const email = document.querySelector("#email-register").value;
     const name = document.querySelector("#name-register").value;
-    const phone = document.querySelector("#phone-register").value;
+    const phone = parseInt(document.querySelector("#phone-register").value);
     const address = document.querySelector("#address-register").value;
     const name_type = document.querySelector("#name_type").textContent;
     let isOrganisation = false;
@@ -56,7 +56,7 @@ document
         email: email,
         password: password,
         name: name,
-        phone: phone,
+        phone_number: phone,
         address: address,
         is_organisation: isOrganisation,
       }),
