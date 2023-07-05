d08e500a758e6078fe406f4145d886247edde04a
ajearle11 <ajearle9@gmail.com>
2023-07-05 21:39:49 +0100

    Added email validation on register

--- client/homepage/homepageRequests.js
+++ client/homepage/homepageRequests.js
@@ -50,38 +50,48 @@ document
     let pattern =
       /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/;
     let result = pattern.test(textTest);
+
+    let emailtext = email;
+    let emailPattern =
+      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    let emailValidation = emailPattern.test(emailtext);
+
     if (!result) {
       alert(
         "Your password must be at least 8 characters, including one lowercase, one uppercase, one special character and a number"
       );
-    }
-
-    const options = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-        name: name,
-        phone_number: phone,
-        address: address,
-        is_organisation: isOrganisation,
-      }),
-    };
-
-    const response = await fetch(
-      "http://localhost:3001/users/register",
-      options
-    );
-    const data = await response.json();
-    console.log(data);
-
-    if (response.status == 201) {
-      window.location.assign("homepage.html");
+    } else if (!emailValidation) {
+      alert("This is not an email address");
     } else {
-      alert(data.error);
+      const options = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          name: name,
+          phone_number: phone,
+          address: address,
+          is_organisation: isOrganisation,
+        }),
+      };
+
+      const response = await fetch(
+        "http://localhost:3001/users/register",
+        options
+      );
+      const data = await response.json();
+      console.log(data);
+
+      if (response.status == 201) {
+        isOrganisation
+          ? window.location.assign("homepage.html")
+          : window.location.assign("homepage.html");
+      } else {
+        alert(data.error);
+      }
     }
   });
